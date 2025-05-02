26fbbdf3e022f01855e94cee4a2a1b3a60cab31f
benjamin-abebe <150177783+benjamin-abebe-02@users.noreply.github.com>
2025-05-02 17:19:23 +0000

    innit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// 
+// Created by: Benjamin Abebe
+// Created on: Feb 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+// process
+function myButtonClicked() {
+  const multiplicandA = parseFloat(document.getElementById("multiplicand").value)
+  const multiplierB = parseFloat(document.getElementById("multiplier").value)
+  let result = 0
+  let count = 0
+
+  while (count < multiplierB) {
+    result += multiplicandA
+    count++
+  }
+
+  // display results
+  document.getElementById("answer").innerHTML = "<p>The answer is:- " + result + ".</p>"
+}
